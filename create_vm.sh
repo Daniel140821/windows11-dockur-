d1bcd24218e095b8f662c13046282a3b0f806564
Daniel140821 <138580504+Daniel140821@users.noreply.github.com>
2025-03-28 15:50:51 +0800

    Create create_vm.sh

---
+++ create_vm.sh
@@ -0,0 +1,10 @@
+sudo apt update
+sudo apt install cpu-checker
+sudo kvm ok
+sudo docker compose up -d
+echo "Winsows 11 VM runing"
+IP_ADDRESS=$(hostname -I | awk '{print $1}')
+each "---------Windows 11 VM---------"
+each "Windows 11 VNC:$IP_ADDRESS:8006"
+each "Windows 11 RDP:$IP_ADDRESS:3389"
+each "-------------------------------"
