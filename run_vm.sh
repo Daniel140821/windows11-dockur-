3065e83bf0ce3a0048be1a7ac9f5149c935c1f4b
Daniel140821 <138580504+Daniel140821@users.noreply.github.com>
2025-03-28 19:05:34 +0800

    Update run_vm.sh

--- run_vm.sh
+++ run_vm.sh
@@ -1,7 +1,7 @@
 sudo docker compose up -d
 echo "Winsows 11 VM runing"
 IP_ADDRESS=$(hostname -I | awk '{print $1}')
-each "---------Windows 11 VM---------"
-each "Windows 11 VNC:$IP_ADDRESS:8006"
-each "Windows 11 RDP:$IP_ADDRESS:3389"
-each "-------------------------------"
+echo "---------Windows 11 VM---------"
+echo "Windows 11 VNC:$IP_ADDRESS:8006"
+echo "Windows 11 RDP:$IP_ADDRESS:3389"
+echo "-------------------------------"
